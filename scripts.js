60e2a0aa8723b3b6a5066cca9dbf01a33c1ca809
name <email>
2020-09-20 20:39:56 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let a=5;
+let b=2;
+
+let usuario='Carlos';
+
+console.log('Operadores matemáticos:');
+
+console.log('a='+a);
+console.log('b='+b);
+
+console.log('a+b='+(a+b));
+console.log('a-b='+(a-b));
+console.log('a*b='+(a*b));
+console.log('a/b='+(a/b));
+console.log('a%b='+(a%b));
+console.log('Hola '+'usuario '+usuario);
+
+console.log(' ');
+console.log('Operadores incremento/decremento:');
+console.log('Post-incremento de a ((a++)+b): '+((a++)+b));
+console.log('Tras la operación a = '+a);
+console.log('Pre-incremento de a ((++a)+b): '+((++a)+b));
+console.log('Tras la operación a = '+a);
+console.log('Post-decremento de a ((a--)+b): '+((a--)+b));
+console.log('Tras la operación a = '+a);
+console.log('Pre-decremento de a ((--a)+b): '+((--a)+b));
+console.log('Tras la operación a = '+a);
+
+console.log(' ');
+console.log('Operadores asignación:');
+console.log('a('+a+') += b('+b+') = '+(a+=b));
+console.log('a('+a+') -= b('+b+') = '+(a-=b));
+console.log('a('+a+') *= b('+b+') = '+(a*=b));
+console.log('a('+a+') /= b('+b+') = '+(a/=b));
+console.log('a('+a+') %= b('+b+') = '+(a%=b));
